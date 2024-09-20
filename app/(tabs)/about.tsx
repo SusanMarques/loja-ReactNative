5172318941ad04e06ad8325cda90dac9a2a95f76
SUSAN KARIN SOARES MARQUES <a95284103@rn.senac.local>
2024-09-20 16:55:37 -0300

    adicionar sobre

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,16 +1,83 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, Linking } from "react-native";
 
-export default function Screen (){
-    return(
+export default function Screen() {
+
+    // Função para abrir o LinkedIn
+    const abrirlink = () => {
+        Linking.openURL('https://www.linkedin.com/in/seu-linkedin/');
+    };
+
+    return (
         <View style={styles.container}>
-            <Text>Fui eu quem fiz :D</Text>
+            <View style={styles.blocoPerfil}>
+                <View style={styles.fotoenome}>
+                <Image
+                    source={require('../../assets/foto.png')}
+                    resizeMode="cover"
+                    style={styles.perfil}
+                />
+                <Text style={styles.nome}> {'<'} SUSAN KARIN /{'>'}</Text>
+                </View>
+                <View style={styles.bloco}>
+                <Text style={styles.descricao}>
+                    Concluinte em licenciatura em informática no IFRN e cursando desenvolvimento de sistemas no Senac/RN {"\n"}
+                    🌱 Atualmente estou aprendendo JavaScript e React Native{"\n"}
+                    💬 HardSkills: JavaScript, PHP, Laravel, React Native, Expo, Electron, React, API rest, Git e Github, PostgreSQL, SQL server, MYSQL, HTML e CSS
+                    {"\n"}
+                    Me encontre nas redes sociais:
+                </Text>
+                <Text style={styles.links}>
+                    LinkedIn:{" "}
+                    <Text style={styles.link} onPress={abrirlink}>
+                        https://www.linkedin.com/in/seu-linkedin/
+                    </Text>
+                </Text>
+                </View>
+            </View>
         </View>
     );
 }
+
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
-        justifyContent: 'center',
+        backgroundColor: '#ffffff'
+    },
+    nome: {
+        padding: 10,
+        fontWeight: '500',
+        position: 'relative',
+        marginTop: 100,
+        color: '#713C87'
+       
+    },
+    descricao: {
+        padding: 20,
+        textAlign: 'justify'
+    },
+    perfil: {
+        width: 150,
+        height: 150,
+        borderRadius: 100
+    },
+    blocoPerfil: {
         alignItems: 'center'
+    },
+    link: {
+        color: 'blue',
+        textDecorationLine: 'underline',
+        
+    },
+    links:{
+        padding: 20
+    },
+    bloco:{
+        backgroundColor: "#CFA4E1",
+        borderRadius: 15
+    },
+    fotoenome:{
+        flexDirection: 'row',
+        
+        
     }
-})
+});
